10b51b8781a541d44ca6ff6e36041e7be38ca4ba
Seetaramayya <3521036+Seetaramayya@users.noreply.github.com>
2024-06-21 07:27:30 +0200

    fixed upload

--- create-zipfile.sh
+++ create-zipfile.sh
@@ -8,4 +8,5 @@ cp -r ../shipper/shipper.py dist/python3/shipper \
   && cd dist/ \
   && zip logzio-cloudwatch lambda_function.py python3/shipper/*
 
-aws s3 cp ./python3/cloudwatch/dist/logzio-cloudwatch.zip s3://maistro-infrastructure/logzio-cloudwatch-maistro.zip
+aws s3 cp ./logzio-cloudwatch.zip s3://maistro-infrastructure/logzio-cloudwatch-maistro.zip
+aws s3 cp ./logzio-cloudwatch.zip s3://pronto-infrastructure/logzio-cloudwatch.zip
